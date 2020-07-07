ed22dbc174ee22c6a51f9c42879ffa9302c5fe79
Patrick LeBoutillier <patrick.leboutillier@gmail.com>
2020-07-07 10:51:14 -0400

    Update jcsdemo.v

--- jcsdemo/jcsdemo.srcs/sources_1/imports/new/jcsdemo.v
+++ jcsdemo/jcsdemo.srcs/sources_1/imports/new/jcsdemo.v
@@ -1,21 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/05/2020 08:56:31 PM
-// Design Name: 
 // Module Name: jcsdemo
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
